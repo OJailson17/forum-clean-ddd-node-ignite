2c31670a99313f566dbb7b157a1df82a67452582
OJailson17 <JaylsonO17@gmail.com>
2023-12-18 22:10:21 -0300

    chore: add answers repository

--- src/domain/use-cases/answer-question-use-case.spec.ts
+++ src/domain/use-cases/answer-question-use-case.spec.ts
@@ -1,11 +1,19 @@
 import { describe, expect, it } from 'vitest';
 import { AnswerQuestionUseCase } from './answer-question-use-case';
+import { AnswersRepository } from '../repositories/answers-repository';
+import { Answer } from '../entities/answer';
+
+const fakeAnswersRepository: AnswersRepository = {
+	create: async (answer: Answer) => {
+		return;
+	},
+};
 
 describe('Answer Question', () => {
-	it('should be able to create an answer', () => {
-		const answerQuestion = new AnswerQuestionUseCase();
+	it('should be able to create an answer', async () => {
+		const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
 
-		const answer = answerQuestion.execute({
+		const answer = await answerQuestion.execute({
 			content: 'New answer',
 			instructorId: '1',
 			questionId: '1',
