d3065c15914c160eba7273871bab855962cfb21b
OJailson17 <JaylsonO17@gmail.com>
2023-12-24 22:58:07 -0300

    chore: add factories to test and use fake data

--- src/domain/forum/application/use-cases/get-question-by-slug-use-case.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug-use-case.spec.ts
@@ -1,8 +1,7 @@
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository';
 import { GetQuestionBySlugUseCase } from './get-question-by-slug-use-case';
-import { UniqueEntityID } from '@/core/entities/unique-entity-id';
-import { Question } from '../../enterprise/entities/question';
 import { Slug } from '../../enterprise/entities/value-objects/slug';
+import { makeQuestion } from 'test/factories/make-question';
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let sut: GetQuestionBySlugUseCase;
@@ -14,20 +13,17 @@ describe('Get Question by Slug', () => {
 	});
 
 	it('should be able to get a question by slug', async () => {
-		const questionCreated = Question.create({
-			content: 'New question',
-			title: 'test title',
+		const newQuestion = makeQuestion({
 			slug: Slug.create('test-title'),
-			authorId: new UniqueEntityID('1'),
 		});
 
-		await inMemoryQuestionsRepository.create(questionCreated);
+		await inMemoryQuestionsRepository.create(newQuestion);
 
 		const { question } = await sut.execute({
 			slug: 'test-title',
 		});
 
 		expect(question.id).toBeTruthy();
-		expect(question.title).toEqual(questionCreated.title);
+		expect(question.title).toEqual(newQuestion.title);
 	});
 });
