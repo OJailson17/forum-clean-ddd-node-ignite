e2632b6d8f80829a84b9e3447912c1890cfb82a1
OJailson17 <JaylsonO17@gmail.com>
2024-01-01 20:47:43 -0300

    test: add tests to delete answer use case

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -7,4 +7,18 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 	async create(answer: Answer): Promise<void> {
 		this.items.push(answer);
 	}
+
+	async delete(answer: Answer) {
+		const itemIndex = this.items.findIndex(item => item.id === answer.id);
+
+		this.items.splice(itemIndex, 1);
+	}
+
+	async findById(id: string) {
+		const question = this.items.find(item => item.id.toString() === id);
+
+		if (!question) return null;
+
+		return question;
+	}
 }
