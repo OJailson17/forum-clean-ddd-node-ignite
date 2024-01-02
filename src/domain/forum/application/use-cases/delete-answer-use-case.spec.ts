97ad4a80dd4ee811dd6b6b2d2c6e1541246496de
OJailson17 <JaylsonO17@gmail.com>
2024-01-01 21:10:35 -0300

    test: add tests to edit question use case

--- src/domain/forum/application/use-cases/delete-answer-use-case.spec.ts
+++ src/domain/forum/application/use-cases/delete-answer-use-case.spec.ts
@@ -12,7 +12,7 @@ describe('Delete Answer', () => {
 		sut = new DeleteAnswerUseCase(inMemoryAnswersRepository);
 	});
 
-	it('should be able to delete a answer', async () => {
+	it('should be able to delete an answer', async () => {
 		const newAnswer = makeAnswer(
 			{
 				authorId: new UniqueEntityID('author-1'),
@@ -30,7 +30,7 @@ describe('Delete Answer', () => {
 		expect(inMemoryAnswersRepository.items).toHaveLength(0);
 	});
 
-	it('should not be able to delete a answer from another user', async () => {
+	it('should not be able to delete an answer from another user', async () => {
 		const newAnswer = makeAnswer(
 			{
 				authorId: new UniqueEntityID('author-1'),
