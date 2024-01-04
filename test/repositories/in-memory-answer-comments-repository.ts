bb43323b57e8be3dd79ebc457e5f699d4801ae4e
OJailson17 <JaylsonO17@gmail.com>
2024-01-03 22:13:40 -0300

    test: add tests to comment on answers use case

---
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -0,0 +1,13 @@
+/* eslint-disable indent */
+import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository';
+import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment';
+
+export class InMemoryAnswerCommentsRepository
+	implements AnswerCommentsRepository
+{
+	public items: AnswerComment[] = [];
+
+	async create(questionComment: AnswerComment) {
+		this.items.push(questionComment);
+	}
+}
