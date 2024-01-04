5e11f4ce2c93be736c84c5827baf681004cbcbbf
OJailson17 <JaylsonO17@gmail.com>
2024-01-03 21:46:04 -0300

    test: add tests to comment on question use case

---
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -0,0 +1,13 @@
+/* eslint-disable indent */
+import { QuestionsCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository';
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment';
+
+export class InMemoryQuestionCommentsRepository
+	implements QuestionsCommentsRepository
+{
+	public items: QuestionComment[] = [];
+
+	async create(questionComment: QuestionComment) {
+		this.items.push(questionComment);
+	}
+}
