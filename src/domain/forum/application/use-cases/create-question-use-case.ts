12e58949393f9bef8a1b46113ee791ea7a31e211
OJailson17 <JaylsonO17@gmail.com>
2024-01-07 22:07:29 -0300

    feat: create aggregate root to add attachments to answers and questions

--- src/domain/forum/application/use-cases/create-question-use-case.ts
+++ src/domain/forum/application/use-cases/create-question-use-case.ts
@@ -2,11 +2,13 @@ import { Either, right } from '@/core/either';
 import { Question } from '../../enterprise/entities/question';
 import { QuestionsRepository } from '../repositories/questions-repository';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment';
 
 interface CreateQuestionRequest {
 	authorId: string;
 	title: string;
 	content: string;
+	attachmentsIds: string[];
 }
 
 type CreateQuestionResponse = Either<
@@ -23,12 +25,22 @@ export class CreateQuestionUseCase {
 		title,
 		content,
 		authorId,
+		attachmentsIds,
 	}: CreateQuestionRequest): Promise<CreateQuestionResponse> {
 		const question = Question.create({
 			title,
 			content,
 			authorId: new UniqueEntityID(authorId),
 		});
+
+		const questionAttachments = attachmentsIds.map(attachmentId => {
+			return QuestionAttachment.create({
+				attachmentId: new UniqueEntityID(attachmentId),
+				questionId: question.id,
+			});
+		});
+
+		question.attachments = questionAttachments;
 
 		await this.questionsRepository.create(question);
 
